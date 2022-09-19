2c0d28ca2f6d7347957f38346a0a2a486c051445
Ivan Kuznetsov <kyzaiv1@yandex.ru>
2022-09-20 00:10:36 +1000

    oawd

---
+++ FLDjangoServer/server/views.py
@@ -0,0 +1,29 @@
+import json
+
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+from .models import Table1
+
+
+# Create your views here.
+def getAll(request):
+    output = []
+    out = Table1.objects.filter(id=1).values()
+    return JsonResponse({"data1": out[0]['data1']})
+
+
+@csrf_exempt
+def insertRow(request):
+    if request.method == 'POST':
+        row = json.loads(request.body)
+        if request.body is None:
+            return JsonResponse({"status": "INVALID INPUT"})
+        origin = models.Table1.objects.filter(id=1).values()[0]
+        origin['data2'] = row['data2']
+        table = Table1(id=1, data1=origin['data1'], data2=origin['data2'], data3=origin['data3'], data4=origin['data4'])
+        table.save()
+        return JsonResponse({"status": "OK"})
+    else:
+        return JsonResponse({"status": "NOT POST"})
